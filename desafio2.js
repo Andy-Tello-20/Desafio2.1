dc82bb6e0a041351072e3040498e1786083ddc1d
Andrés Tello <andrest911@gmail.com>
2023-10-07 17:05:15 -0300

    ultimos cambios

--- desafio2.js
+++ desafio2.js
@@ -8,9 +8,9 @@ class ProductManager {
 
     constructor(path) {
         this.path = path
-        
 
-        
+
+
     }
     //Metodos de la clase: Éstos debe poder agregar, consultar, modificar y eliminar un producto
 
@@ -30,15 +30,15 @@ class ProductManager {
         const { title, description, price, thumbnail, code, stock } = data
         //validamos (por las dudas)
         if (!title || !description || !price || !thumbnail || !code || !stock) {
-             console.error('Todos los campos son obligatorios')
-             return
+            console.error('Todos los campos son obligatorios')
+            return
         }
 
         //Antes de adherir un nuevo producto necesito saber si ya existe un archivo 
 
         const existFile = await request(this.path)
         const newProduct = {
-            id:existFile.length + 1, //cuenta la cantidad de elementos o "productos" dentro del archivo y añade 1 unidad en el id
+            id: existFile.length + 1, //cuenta la cantidad de elementos o "productos" dentro del archivo y añade 1 unidad en el id
             title,
             description,
             price,
@@ -46,74 +46,76 @@ class ProductManager {
             code,
             stock
         }
-        
+
 
         //el archivo ya tendria el nuevo contenido disponible pára ingresar
         existFile.push(newProduct)
 
         //guardar el archivo
 
-        await saveinFile (this.path,existFile)
+        await saveinFile(this.path, existFile)
 
-     
+
 
     }
 
-     async getProducts () {
+    async getProducts() {
         // si request es una promesa debemos llamar a (getProducts) como una funcion asincrona utilizando "await" para esperar 
 
         const mostrarProductos = await request(this.path)
         return console.log(mostrarProductos)
-     }
+    }
 
 
-     async getProductById (id) {
+    async getProductById(id) {
 
-        const lectura= await request(this.path)
-        const buscarPorID = lectura.find((i)=>{
-            return i.id===id
+        const lectura = await request(this.path)
+        const buscarPorID = lectura.find((i) => {
+            return i.id === id
         })
 
-        if(buscarPorID){
+        if (buscarPorID) {
             console.log("el producto encontrado fue:", buscarPorID)
-            
-        }else{
+
+        } else {
             console.log("no se encontro ningun producto")
         }
 
-     }
+    }
 
-     async updateProduct (id,campo){
-        const lectura= await request(this.path)
-        
-        const buscarIndice = lectura.findIndex((i)=>{
-           return i.id===id
+    async updateProduct(id, campo) {
+        const lectura = await request(this.path)
+
+        const buscarIndice = lectura.findIndex((i) => {
+            return i.id === id
         })
-        
 
-        const {title}=lectura[buscarIndice]
-        if(title){
-            lectura[buscarIndice].title=campo
+
+        const { title } = lectura[buscarIndice]
+        if (title) {
+            lectura[buscarIndice].title = campo
+            console.log("se ha cambiado el titulo del producto")
         }
 
-         await saveinFile (this.path,lectura)
+        await saveinFile(this.path, lectura)
 
 
-     }
+    }
 
 
-     async deleteProduct(id){
+    async deleteProduct(id) {
 
-        const lectura  = await request(this.path)
-        const buscarIndicePorID = lectura.findIndex((i)=>{
-            return i.id===id
+        const lectura = await request(this.path)
+        const buscarIndicePorID = lectura.findIndex((i) => {
+            return i.id === id
         })
-        if(buscarIndicePorID){
-        lectura.splice([buscarIndicePorID],1)
+        if (buscarIndicePorID) {
+            lectura.splice([buscarIndicePorID], 1)
+            console.log("se ha eliminado un producto")
         }
-        await saveinFile (this.path,lectura)
-    
-    
+        await saveinFile(this.path, lectura)
+
+
     }
 }
 
@@ -126,46 +128,47 @@ const request = async (path) => {
 
     } else {
         // si existe, lee el archivo existente y retorna su contenido en formato JSON (parse= un objeto dentro de un array)
-        try{
-        const content = await fs.promises.readFile(path, 'utf-8')
-        return JSON.parse(content)
-        }catch(error){
-            console.log("ha ocurrido un error",error)
+        try {
+            const content = await fs.promises.readFile(path, 'utf-8')
+            return JSON.parse(content)
+        } catch (error) {
+            console.log("ha ocurrido un error", error)
         }
     }
 }
 
 
 
-const saveinFile = (path, data) => {
+const saveinFile = async (path, data) => {
     const content = JSON.stringify(data, null, '\t')
-    try{
-        return fs.promises.writeFile(path, content, 'utf-8')
-    }catch(error){
-        console.log("ha ocurrido un error",error)
+    try {
+        await fs.promises.writeFile(path, content, 'utf-8')
+        console.log("Se han guardado los cambios")
+    } catch (error) {
+        console.log("ha ocurrido un error", error)
     }
-    
-  }
+
+}
 
 
 
-  async function test() {
+async function test() {
     const product1 = new ProductManager("./products.json")
-  
+
     const data = {
-      title: "mesa",
-      description: "de madera",
-      price: 5000,
-      thumbnail: "img1",
-      code: 1234,
-      stock: 5,
+        title: "mesa",
+        description: "de madera",
+        price: 5000,
+        thumbnail: "img1",
+        code: 1234,
+        stock: 5,
     }
-  
+
     await product1.addProduct(data)
     await product1.getProducts()
     await product1.getProductById(5)
-    await product1.updateProduct (7,"cama")
+    await product1.updateProduct(7, "cama")
     await product1.deleteProduct(3)
-  }
-  
-  test()
+}
+
+test()
